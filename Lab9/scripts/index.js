beb4a1765ecb4cc000efcb7bac198185eed16493
Max Donchenko <wereya2@yandex.ru>
2016-07-17 01:12:25 +0300

    Lab 9: add page structure and basic style

--- Lab9/scripts/index.js
+++ Lab9/scripts/index.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
-	// code will be here
+	// submit ID
+	$("#submitID").click(function(){
+		var id = +$("#inputID").val();
+		if (id) {
+			$("#inputID").val("");
+			$("#friends-panel h3").hide();
+			$("#friends-panel .users-list").empty();
+			loadData(id);
+		}
+	});
+	$("#inputID").keypress(function (e) {
+		if (e.which == 13) {
+			$("#submitID").click();
+			return false;
+		}
+	});
+
+	$("#new-hobbie-add").click(function(){
+		var hobbie = $("#new-hobbie-input").val();
+		if (hobbie) {
+			$("#new-hobbie-input").val("");
+			$("#user-panel .hobbies ul").append("<li>" + hobbie + "</li>");
+		}
+	});
+	$("#new-hobbie-input").keypress(function (e) {
+		if (e.which == 13) {
+			$("#new-hobbie-add").click();
+			return false;
+		}
+	});
+
+	function loadData(id) {
+		// TODO: implementation
+	}
 });
